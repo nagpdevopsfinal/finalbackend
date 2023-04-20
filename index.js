13c4873c3b3955ee2dcfa711b1147d80284a651e
Amit Sharma <amitsharma@balsambrands.com>
2023-04-20 23:09:52 +0530

    adding extra code

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const routes = require('./routes');
 let port = process.env.PORT || 4000;
 
 app.use('/api/v1', routes);
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`The server is listening on port ${port}`)
 })
+app.use((request, response, next) => {
+  if (request.path === "/") {
+    response.status(200).send({ message: "ok" });
+  } else {
+    next();
+  }
+});
